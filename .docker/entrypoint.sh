a651ab9f2791cd984564bb06dabf9523f2d0ded8
Wanderson <wanderson.vieira@movida.com>
2022-09-08 20:22:07 -0300

    Criando comando do php storage no entrypoint

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -2,10 +2,10 @@
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
 chown -R www-data:www-data .
-chown -R mysql:mysql /var/lib/mysql/code_micro_videos
 chown -R mysql:mysql .
 composer install
 php artisan key:generate
 php artisan migrate
+php artisan storage:link
 
 php-fpm
